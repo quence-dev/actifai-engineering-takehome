6cb809d5a467e8fe5b0a5359d5bd77800fc49ba7
Spencer <quence@bu.edu>
2025-04-10 21:23:58 -0400

    fix optional params

--- db/get_avg_sales_by_month.sql
+++ db/get_avg_sales_by_month.sql
@@ -5,6 +5,10 @@ WITH grouped_users AS (
     u.name AS user_name
   FROM user_groups ug
   LEFT JOIN users u ON u.id = ug.user_id
+  WHERE CASE 
+    WHEN ${user_id}::INTEGER IS NOT NULL THEN u.id = ${user_id}
+    ELSE TRUE
+  END
 ), grouped_sales AS (
   SELECT 
     g.id AS group_id,
@@ -16,9 +20,8 @@ WITH grouped_users AS (
   FROM groups g
   LEFT JOIN grouped_users gu ON gu.group_id = g.id
   LEFT JOIN sales s ON s.user_id = gu.user_id
-  WHERE 
-    s.date >= ${begin_date}::DATE AND
-    s.date < ${end_date}::DATE
+  WHERE s.date >= ${begin_date}::DATE
+    AND s.date <= ${end_date}::DATE
   GROUP BY g.id, g.name, gu.user_id, gu.user_name
 ) SELECT 
   group_name,
@@ -26,4 +29,11 @@ WITH grouped_users AS (
   total_revenue,
   average_revenue
 FROM grouped_sales
+WHERE
+  CASE WHEN ${min_amount}::BIGINT IS NOT NULL THEN total_revenue >= ${min_amount}::BIGINT
+    ELSE TRUE
+  END
+  AND CASE WHEN ${max_amount}::BIGINT IS NOT NULL THEN total_revenue <= ${max_amount}::BIGINT
+    ELSE TRUE
+  END
 ORDER BY group_name, user_name;
